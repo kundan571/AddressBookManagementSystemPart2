2704962cf5ecb82db9ac968b65e007c4a5b073d8
kundan571 <kundanchauhan1997@gmail.com>
2023-02-16 23:25:33 +0530

    Added: Ability to add person Details

--- src/com/addressbookmanagement/AddressBook.java
+++ src/com/addressbookmanagement/AddressBook.java
@@ -1,8 +1,31 @@
 package com.addressbookmanagement;
 
-public class AddressBook  {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBook extends ContactPerson {
     public static void main(String[] args) {
-        System.out.println("Welcome To Address Book Management System Program");
+        AddressBook newPerson = new AddressBook();
+        ArrayList<AddressBook> person = new ArrayList<>();
+        System.out.println("Welcome to Address Book Program");
+        Scanner scanner = new Scanner(System.in);
+        //Add person address
+        System.out.println("Enter first name");
+        newPerson.setFirstName(scanner.nextLine());
+        System.out.println("Enter last name");
+        newPerson.setLastName(scanner.nextLine());
+        System.out.println("Enter address");
+        newPerson.setAddress(scanner.nextLine());
+        System.out.println("Enter city");
+        newPerson.setCity(scanner.nextLine());
+        System.out.println("Enter state");
+        newPerson.setState(scanner.nextLine());
+        System.out.println("Enter zip code");
+        newPerson.setZip(scanner.nextInt());
+        System.out.println("Enter phone number");
+        newPerson.setPhoneNumber(scanner.nextLong());
+        person.add(newPerson);
+
+        System.out.println(person);
     }
 }
-
